4bb8d11416d667ef9f43158f4878061d4b5504e9
mandytrinh <saphiric@gmail.com>
2017-02-06 13:59:25 -0800

    problem 2 completed

--- lab10/expr.py
+++ lab10/expr.py
@@ -110,7 +110,10 @@ class Name(Expr):
         ...     print('Exception raised!')
         Exception raised!
         """
-        "*** YOUR CODE HERE ***"
+
+        if self.string not in env:
+            raise NameError("name {} is not defined".format(self.string))
+        return env[self.string]
 
     def __str__(self):
         return self.string
